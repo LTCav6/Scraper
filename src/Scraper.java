576ebc29a87805f91ba64d3b40bcb11aff003aeb
System Administrator <root@mbp.myfiosgateway.com>
2021-10-22 23:40:55 -0400

    updates to the scrape wait times

--- src/Scraper.java
+++ src/Scraper.java
@@ -5,14 +5,18 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+import java.util.function.DoubleToIntFunction;
 
 import static java.rmi.server.LogStream.log;
 
 public class Scraper {
 
-    private String seedUrl = "https://en.wikipedia.org/"; //let's use this as the default in case one isn't provided
+    private String seedUrl = "https://cnn.com/"; //let's use this as the default in case one isn't provided
     LinkedList<String> pages = new LinkedList<String>();
     private int errors = 0;
+    private int pageWaitTimeMin = 6;
+    private int pageWatTimeMax = 240;
 
     public Scraper (){
 
@@ -32,8 +36,14 @@ public class Scraper {
     public void scrape(){
         Document doc = null;
         try {
+            double wait = Math.random() * (pageWatTimeMax - pageWaitTimeMin + 1) + pageWaitTimeMin;
+            // delay the scrape randomly to avoid being ip blocked
+            System.out.println(String.format("value is %03d",(int)wait));
+            //convert wait to integer for TimeUnit
+            TimeUnit.SECONDS.sleep((int)wait);
             doc = Jsoup.connect(this.seedUrl).get();
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
+            //this will catch an exception with the page or with our sleep timer
             e.printStackTrace();
         }
         System.out.println(doc.title());
